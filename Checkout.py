2730d9f8d3410cdc1f50e5082e4313638738e0f0
Toni Mitrevski <toni@stativa.com.mk>
2018-07-15 20:14:32 +0200

    refactor code

--- Checkout.py
+++ Checkout.py
@@ -1,16 +1,55 @@
 class Checkout:
-    price = {}
-    total = 0
+    class Discount:
+        def __init__(self, nbrItems, price):
+            self.nbrItems = nbrItems
+            self.price = price
 
     def __init__(self):
-        self.price = {}
-        self.total = 0
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
 
-    def add_item_price(self, item, price):
-        self.price[item] = price
 
-    def add_item(self, item):
-        self.total += self.price[item]
+    def addDiscount(self, item, nbrOfItems, price):
+        discount = self.Discount(nbrOfItems, price)
+        self.discounts[item] = discount
 
-    def calculate_total(self):
-        return self.total
+
+    def addItemPrice(self, item, price):
+        self.prices[item] = price
+
+    def addItem(self, item):
+        if item not in self.prices:
+            raise Exception("Bad Item")
+
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
+    def calculateTotal(self):
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.calculateItemTotal(item, cnt)
+        return total
+
+    def calculateItemTotal(self, item, cnt):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if cnt >= discount.nbrItems:
+                total += self.calculateItemDiscountedTotal(item, cnt, discount)
+            else:
+                total += self.prices[item] * cnt
+        else:
+            total += self.prices[item] * cnt
+
+        return total
+
+    def calculateItemDiscountedTotal(self, item, cnt, discount):
+        total = 0
+        nbrOfDiscounts = cnt / discount.nbrItems
+        total += nbrOfDiscounts * discount.price
+        remaining = cnt % discount.nbrItems
+        total += remaining * self.prices[item]
+        return total
--- TestCheckout.py
+++ TestCheckout.py
@@ -1,22 +1,32 @@
 import pytest
 from Checkout import Checkout
 
-
 @pytest.fixture()
 def checkout():
-    co = Checkout()
-    return co
+    checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    return checkout
 
+def test_CanCalculateTotal(checkout):
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
 
-def test_can_total(checkout):
-    checkout.add_item_price('a', 1)
-    checkout.add_item('a')
-    assert checkout.calculate_total() == 1
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
 
+def test_canAddDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
 
-def test_get_correct_total_with_multiple_items(checkout):
-    checkout.add_item_price('a', 1)
-    checkout.add_item_price('b', 2)
-    checkout.add_item('a')
-    checkout.add_item('b')
-    assert checkout.calculate_total() == 3
+def test_canApplyDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
+
+def test_ExceptionWithBadItem(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
